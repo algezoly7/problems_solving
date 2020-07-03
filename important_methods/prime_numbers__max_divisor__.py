00beac96b12a8e4bff2769fd0d5cabccc09df9ca
algezoly7 <63818969+algezoly7@users.noreply.github.com>
2020-07-03 11:11:58 -0700

    Update prime_numbers__max_divisor__.py

--- important_methods/prime_numbers__max_divisor__.py
+++ important_methods/prime_numbers__max_divisor__.py
@@ -1,10 +1,17 @@
-#for more informations copy this link: https://youtu.be/2p3kwF04xcA .
-#to find out whether a number is a prime one .
+#for more informations copy this link: https://youtu.be/2p3kwF04xcA.
+#to find out whether a number is a prime one.
 import math
+import time
 n = int(input())
-max_divisor = math.floor(math.sqrt(n))
+max_divisor = math.ceil(math.sqrt(n))
 tracker = True
-for i in range(3, max_divisor, 2):
-    if(n % i == 0):
-        tracker = False
+#all prime numbers are odd numbers.
+if(n != 2 and n % 2 == 0):
+    tracker = False
+#odd numbers can't have even numbers as coefficients.
+else:
+    for i in range(3, max_divisor + 1, 2):
+        if(n % i == 0):
+            tracker = False
+            break
 print(tracker)
